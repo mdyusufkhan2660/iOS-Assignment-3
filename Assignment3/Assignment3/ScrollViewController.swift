2485e6c34826d471b4a3f95feab3b787ad7537da
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-11-22 10:43:11 -0500

    Added button navigation on home screen

---
+++ Assignment3/Assignment3/ScrollViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollViewController.swift
+//  Assignment3
+//
+//  Created by Yusuf Khan on 2022-11-22.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Scroll View Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
